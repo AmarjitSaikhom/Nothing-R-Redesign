b9f4d34e4ad801adec7bd5f846ff658ddaf729b9
Amarjit Saikhom <shaikhomamarjit@gmail.com>
2025-07-25 03:31:18 +0530

    fix product page

--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -4,9 +4,9 @@ const ProductCards = ({ name, image, price }) => {
     return (
         <div className='product_card'>
             <div className="product-image">
-                <img src={image} alt={name} />
+                <img src={image} alt={name} loading="lazy" />
             </div>
-            
+
             <div className="product-title">
                 <h3>{name}</h3>
                 <i className="ri-shopping-bag-line"></i>
